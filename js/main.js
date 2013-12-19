14087ab1261d5f690b8e4a5cf7a3c6e7bc706d74
Laney <laney.bevill@gmail.com>
2013-12-18 18:10:57 -0600

    update index and js/main

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert('hi');
+
+
+var itemTemplate = $('#templates .item').html();
+
+var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+$('#store').append(newItemHtml);
+
+var items = [
+  { name: 'NAME', price: 0 },
+  { name: 'NAME', price: 0 },
+  { name: 'NAME', price: 0 }
+];
